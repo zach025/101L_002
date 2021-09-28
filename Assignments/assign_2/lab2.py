e9faa7effa0dbe32f73f39f46c37fcdd71f4d6d2
zach025 <zof6b@mail.umkc.edu>
2021-09-27 19:30:40 -0500

    Update lab2.py

--- Assignments/assign_2/lab2.py
+++ Assignments/assign_2/lab2.py
@@ -8,7 +8,7 @@ while play_again == 'Y':
     print('please think of a number between and including 1 to 100.\n')
 
     remain_3 = int(input('What is the remainder when your number is divided by 3?'))
-    while remain_3 > 3:
+    while remain_3 > 2:
         print('The valued entered must be less than 3')
         remain_3 = int(input('What is the remainder when your number is divided by 3?'))
     while remain_3 < 0:
@@ -16,8 +16,20 @@ while play_again == 'Y':
         remain_3 = int(input('What is the remainder when your number is divided by 3?'))
 
     remain_5 = int(input('What is the remainder when your number is divided by 5?'))
+    while remain_5 > 4:
+        print('The value entered must be less than 5')
+        remain_5 = int(input('What is the remainder when your number is divided by 5?'))
+    while remain_5 < 0:
+        print('The value entered must be 0 or greater')
+        remain_5 = int(input('What is the remainder when your number is divided by 5?'))
 
     remain_7 = int(input('What is the remainder when your number is divided by 7?'))
+    while remain_7 > 6:
+        print('The value entered must be less than 7')
+        remain_7 = int(input('What is the remainder when your number is divided by 7?'))
+    while remain_7 < 0:
+        print('The value entered must be 0 or greater')
+        remain_7 = int(input('What is the remainder when your number is divided by 7?'))
 
     for i in range(101):
         if (i % 3 == remain_3 and i % 5 == remain_5 and i % 7 ==remain_7):
